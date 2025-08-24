25f06efff1161e4d00e6ba2b466cdd640893b32a
שליו כץ <shalevkatz@MacBookAir.lan>
2025-08-24 12:07:16 +0300

    remove image line from index

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ import { styles } from "../styles/auth.styles";
 export default function Index() {
   return (
     <View style={styles.container}>
+      {/* Add your components here */}
     </View>
   );
 }
